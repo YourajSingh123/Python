e1c49eaf4365ca16304c800d8a4fb2c716f09b47
YourajSingh123 <rajyou0123@gmail.com>
2026-05-07 23:18:29 +0530

    add

---
+++ Hello.py
@@ -0,0 +1,25 @@
+print("Hello world")
+print("let's go YUVI")
+
+#Variables
+name = "Youraj"
+age = 20
+print(name)
+print(age)
+print(type(name))
+print(type(age))
+
+#Input from user
+a = input("name:")
+b = int(input("marks:"))
+c = float(input("price:"))
+
+#Type conversion
+x, y = 1, 2.4
+sum = x + y
+print(sum)
+
+#Type casting
+num1 = 5
+num2 = "2"
+num3 = int(num2)
