a1fdae9ca34ba2ca062a537dfdeb945677a9dc5c
YourajSingh123 <rajyou0123@gmail.com>
2026-05-07 23:28:45 +0530

    hello

---
+++ Operators.py
@@ -0,0 +1,30 @@
+a = int(input("enter the 1st num:")) 
+b = int(input("enter the 2nd num:"))
+
+#Arithmetic Operator
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+
+#Relational Operator
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
+
+#Logical Operator
+c=True
+d=False
+print(c and d)
+print(c or d)
+print(not c)
+
+#Assignment operator
+a+=4
+print(a)
+a*=2
+print(a)
+b-=5
+print(b)
