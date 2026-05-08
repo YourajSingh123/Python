6e17504cb351b7cd6948e8367a7861bc0ca841b4
YourajSingh123 <rajyou0123@gmail.com>
2026-05-08 11:05:40 +0530

    add new file

---
+++ Practice2.py
@@ -0,0 +1,16 @@
+#1
+sports = []
+s1 = input("enter 1st fav sport:")
+s2 = input("enter 2nd fav sport:")
+s3 = input("enter 3rd fav sport:")
+
+sports.append(s1)
+sports.append(s2)
+sports.append(s3)
+
+print(sports)
+
+#2
+random = ("Ram", 1, 9, "Ram", "Ram", 9, 8.7)
+print(random.count("Ram"))
+print(random.count(9))
